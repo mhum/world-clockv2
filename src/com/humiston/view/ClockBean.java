690e8be04453a80d35ccd5d1e8302b30afb4eea3
Michael <Michael@MichaelHumiston.com>
2014-05-15 21:54:00 -0400

    Added list of offices

--- src/com/humiston/view/ClockBean.java
+++ src/com/humiston/view/ClockBean.java
@@ -1,6 +1,8 @@
 package com.humiston.view;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -8,29 +10,40 @@ import javax.faces.bean.ViewScoped;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+
+import com.humiston.dto.OfficeDto;
  
 @ManagedBean
 @ViewScoped
 public class ClockBean implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private static final DateTimeFormatter fmt = DateTimeFormat.forPattern("h:mm:ss a");
-	private String date;
+	private List<OfficeDto> offices = new ArrayList<>();
 	
 	public ClockBean(){
-    	DateTime dt = new DateTime();   	
-    	date = fmt.print(dt);
+    	DateTime dt = new DateTime();
+		OfficeDto office = new OfficeDto();
+    	office.setTime(fmt.print(dt));
+    	offices.add(office);
+
+    	office = new OfficeDto();
+    	office.setTime(fmt.print(dt));
+    	offices.add(office);
 	}
  
-    public String getDate() {
-        return date;
+    public List<OfficeDto> getOffices() {
+        return offices;
     }
  
-    public void setDate(String date) {
-        this.date = date;
+    public void setOffices(List<OfficeDto> offices) {
+        this.offices = offices;
     }
     
     public void update(){
     	DateTime dt = new DateTime();
-    	date = fmt.print(dt);
+    	for (int x=0;x<offices.size();x++){
+    		OfficeDto office = offices.get(x);
+    		office.setTime(fmt.print(dt));
+    	}
     }
 }
